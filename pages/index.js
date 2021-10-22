656922b386761c1d9fd3cc6f6ce39213a422eb01
Pavol Kosc <pavol.kosc@hotovo.com>
2021-10-22 19:43:00 +0200

    Finished basic styling

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,11 @@
-import styles from '../styles/Home.module.css'
-import Link from 'next/link';
+import EventList from "../components/events/event-list";
+import {getFeaturedEvents} from "./events/dummy-data";
 
-export default function Home() {
-  return (
-    <div className={styles.container}>
-      <h1>Hello Next World</h1>
-        <ul>
-            <li>
-                <Link href="/portfolio">Portfolio</Link>
-            </li>
-            <li>
-                <Link href="/clients">Clients</Link>
-            </li>
-        </ul>
+function HomePage() {
+    const featuredEvents = getFeaturedEvents();
+    return <div>
+        <EventList items={featuredEvents}/>
     </div>
-  )
 }
+
+export default HomePage;
