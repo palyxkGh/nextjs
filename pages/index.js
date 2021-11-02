db638032c8710ede0aef4a221a1a358e00220ec9
Pavol Kosc <palyxkgh>
2021-11-02 19:14:04 +0100

    Finished preRendering&dataLoad

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,39 @@
 import styles from '../styles/Home.module.css'
 import Link from 'next/link';
+import path from 'path';
+import fs from 'fs/promises';
 
-export default function Home() {
-  return (
-    <div className={styles.container}>
-      <h1>Hello Next World</h1>
+export default function Home(props) {
+    const { products } = props;
+    return (
         <ul>
-            <li>
-                <Link href="/portfolio">Portfolio</Link>
-            </li>
-            <li>
-                <Link href="/clients">Clients</Link>
-            </li>
+            {products.map(product => <li key={product.id}><Link href={`/${product.id}`} key={product.id}>{product.title}</Link></li>)}
         </ul>
-    </div>
-  )
+    )
+}
+
+export async function getStaticProps() {
+    console.log('-> regenerating here ...');
+    const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json');
+    const jsonData = await fs.readFile(filePath);
+    const data = JSON.parse(jsonData);
+
+    if(!data){
+        return {
+            redirect: {
+                destination: '/no-data'
+            }
+        }
+    }
+
+    if(data.products.length === 0){
+        return {notFound: true}
+    }
+    return {
+        props: {
+            products: data.products
+        },
+        //ISR
+        revalidate: 10,
+    };
 }
