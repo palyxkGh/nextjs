d32a554fec58e424d19221a96d8689c75a311e8b
Pavol Kosc <pavol.kosc@hotovo.com>
2021-11-13 16:18:50 +0100

    setUp

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,9 @@
-import styles from '../styles/Home.module.css'
-import Link from 'next/link';
-
-export default function Home() {
+function HomePage() {
   return (
-    <div className={styles.container}>
-      <h1>Hello Next World</h1>
-        <ul>
-            <li>
-                <Link href="/portfolio">Portfolio</Link>
-            </li>
-            <li>
-                <Link href="/clients">Clients</Link>
-            </li>
-        </ul>
+    <div>
+      <h1>The Home Page</h1>
     </div>
-  )
+  );
 }
+
+export default HomePage;
