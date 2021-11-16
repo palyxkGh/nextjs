f858aefaca3713c94c4d553fdbc198a957211308
Pavol Kosc <pavol.kosc@hotovo.com>
2021-11-16 16:04:28 +0100

    added apiRoutes & update

--- pages/api/feedback.js
+++ pages/api/feedback.js
@@ -1,17 +1,17 @@
 import fs from 'fs';
 import path from 'path';
 
-function buildFeedbackPath () {
+export function buildFeedbackPath () {
     return path.join(process.cwd(), 'data', 'feedback.json');
 }
 
-function extractFeedback(filePath) {
+export function extractFeedback(filePath) {
     const fileData = fs.readFileSync(filePath);
     const data = JSON.parse(fileData);
     return data;
 }
 
-function handler(req, res) {
+export function handler(req, res) {
     if(req.method === 'POST') {
         const email = req.body.email;
         const feedbackText = req.body.text;
---
+++ pages/feedback/index.js
@@ -0,0 +1,41 @@
+import {buildFeedbackPath, extractFeedback} from '../api/feedback';
+import {Fragment, useState} from 'react';
+
+function FeedbackPage(props) {
+    const [feedbackData, setFeedbackData] = useState();
+
+    function loadFeedbackHandler(id) {
+        console.log("-> id", id);
+    fetch(`/api/${id}`) // /api/some-feedback-id
+        .then(response =>
+            response.json()
+        )
+        .then(data => {
+            setFeedbackData(data.feedBack);
+        });
+    }
+
+    return (
+        <Fragment>
+            {feedbackData && <p>{feedbackData.email}</p>}
+            <ul>
+                {props.feedbackItems.map(item => <li id={item.id}>
+                    {item.text}
+                    <button onClick={loadFeedbackHandler.bind(null, item.id)}>Show Details</button>
+                </li>)}
+            </ul>
+        </Fragment>
+    );
+}
+
+export async function getStaticProps() {
+    const filePath = buildFeedbackPath();
+    const data = extractFeedback(filePath);
+    return {
+        props: {
+            feedbackItems: data
+        }
+    };
+};
+
+export default FeedbackPage;
